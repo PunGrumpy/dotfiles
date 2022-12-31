f7c7deb0e56fa55242de2d52ad83e36315117eae
PunGrumpy <PunGrumpy+108584943@users.noreply.github.com>
2022-12-31 18:23:58 +0700

    add ignore files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,15 @@
-require('craftzdog.base')
-require('craftzdog.highlights')
-require('craftzdog.maps')
-require('craftzdog.plugins')
+require('grumpy.base')
+require('grumpy.highlights')
+require('grumpy.maps')
+require('grumpy.plugins')
 
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
 
 if is_mac then
-  require('craftzdog.macos')
+  require('grumpy.macos')
 end
 if is_win then
-  require('craftzdog.windows')
+  require('grumpy.windows')
 end
