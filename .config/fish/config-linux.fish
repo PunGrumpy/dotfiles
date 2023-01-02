30b0803708704abb16f473b156fa2b90a3113967
PunGrumpy <PunGrumpy+108584943@users.noreply.github.com>
2023-01-02 09:35:32 +0700

    fix(*): add path homebrew for linux

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -2,3 +2,12 @@ if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
 end
+
+if test -x /home/linuxbrew/.linuxbrew/bin/brew
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
+
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
