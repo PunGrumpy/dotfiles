35622a38dbdadefc75ad07f2df24594affb7f377
PunGrumpy <PunGrumpy+108584943@users.noreply.github.com>
2023-01-02 19:42:45 +0700

    feat(sh): add ask before links

--- .config/zsh/links.sh
+++ .config/zsh/links.sh
@@ -1,3 +1,8 @@
+# Color definitions
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Link funtion
 link() {
     if [ -f "$1" ]; then
@@ -8,8 +13,25 @@ link() {
     fi
 }
 
-# Link files
-link ~/.config/zsh/rc.zsh ~/.zshrc
+# Check if the file exists, if it ask to link it, if not ask to create it
+ask() {
+    if [ -f "$1" ]; then
+        echo "${GREEN}$1 exists${NC}"
+        echo "${GREEN}Do you want to link it? (y/n)${NC}"
+        read -r answer
+        if [ "$answer" = "y" ]; then
+            link "$1" "$2"
+        fi
+    else
+        echo "${RED}$1 does not exist${NC}"
+        echo "${RED}Do you want to create it? (y/n)${NC}"
+        read -r answer
+        if [ "$answer" = "y" ]; then
+            touch "$1"
+        fi
+    fi
+}
 
-# Link powerlevel10k
-link ~/.config/zsh/p10k.zsh ~/.p10k.zsh
+# Link files rc.zsh and p10k.zsh to .zshrc and .p10k.zsh
+ask "$HOME/.config/zsh/rc.zsh" "$HOME/.zshrc"
+ask "$HOME/.config/zsh/p10k.zsh" "$HOME/.p10k.zsh"
