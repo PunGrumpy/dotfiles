f9af6728bf48d9cabfd18de39d0e0bb0d3a17f22
PunGrumpy <PunGrumpy@$(git config --get remote.origin.url | sed -e 's/.*@//' -e 's/:.*//').com>
2023-01-03 19:29:15 +0700

    feat(pwd): add link.ps1 and readme

---
+++ .config/powershell/README.md
@@ -0,0 +1,9 @@
+# PowerShell
+
+## Setup
+
+1. Install [PowerShell](https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.1)
+2. Install Terminal Emulator (e.g. [Windows Terminal](https://www.microsoft.com/en-us/p/windows-terminal/9n0dx20hk701?activetab=pivot:overviewtab))
+3. Install [Oh-My-Posh](https://ohmyposh.dev/)
+4. Install [Nerd Fonts](https://www.nerdfonts.com/)
+5. Run `link.ps1` to create symbolic link
---
+++ .config/powershell/link.ps1
@@ -0,0 +1,48 @@
+# Windows Terminal
+
+# Check oh-my-posh is installed and ask to install it if not
+if (!(Get-Module oh-my-posh -ListAvailable)) {
+    Write-Host "Oh My Posh is not installed. Do you want to install it? (y/n)"
+    $answer = Read-Host
+    if ($answer -eq "y") {
+        winget install JanDeDobbeleer.OhMyPosh --scope user
+        Install-Module posh-git -Scope CurrentUser
+    } else {
+        Write-Host "Oh My Posh is not installed. You can install it later."
+    }
+}
+
+# Check current path is the dotfiles/.config/powershell folder and ask to change it if not
+if ((pwd).Path -ne (pwd).Path + "\.dotfiles\.config\powershell") {
+    Write-Host "You are not in the dotfiles/.config/powershell folder. Do you want to change it? (y/n)"
+    $answer = Read-Host
+    if ($answer -eq "y") {
+        cd .dotfiles\.config\powershell
+    } else {
+        Write-Host "You are not in the dotfiles/.config/powershell folder. You can change it later."
+    }
+}
+
+# Function link
+Link() {
+    $source = $args[0]
+    $target = $args[1]
+
+    if (Test-Path $target) {
+        Remove-Item $target
+    }
+
+    New-Item -ItemType SymbolicLink -Path $target -Value $source
+}
+
+# Create a symbolic link to the profile file
+# New-Item -ItemType SymbolicLink -Path $PROFILE -Value ".dotfiles\powershell\Microsoft.PowerShell_profile.ps1"
+Link (pwd).Path + "\Microsoft.PowerShell_profile.ps1" $PROFILE 
+
+# Create a symbolic link to the theme of Oh My Posh
+# New-Item -ItemType SymbolicLink -Path $env:POSH_THEMES_PATH -Value ".dotfiles\powershell\grumpy.omp.json"
+Link (pwd).Path + "\grumpy.omp.json" $env:POSH_THEMES_PATH
+
+# Create a symbolic link to the theme of Windows Terminal
+# New-Item -ItemType SymbolicLink -Path $env:LOCALAPPDATA\Microsoft\Windows Terminal\settings.json -Value ".dotfiles\windows-terminal\settings.json"
+Link (pwd).Path + "\settings.json" "$env:LOCALAPPDATA\Microsoft\Windows Terminal\settings.json"
