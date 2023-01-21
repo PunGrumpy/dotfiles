3060626d906dcec568128653753295ba922eee6f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-01-21 19:31:33 +0700

    fix(nvim): enable plugin discord rpc

--- .config/nvim/lua/grumpy/plugins.lua
+++ .config/nvim/lua/grumpy/plugins.lua
@@ -47,7 +47,7 @@ packer.startup(function(use)
   })
   use 'akinsho/nvim-bufferline.lua'
   -- use 'github/copilot.vim'
-  -- use 'andweeb/presence.nvim'
+  use 'andweeb/presence.nvim'
 
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim' -- For git blame & browse
