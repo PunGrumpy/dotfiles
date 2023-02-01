a05d6e209dc407a4bc355575124405ce24700717
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-02-01 14:56:59 +0700

    feat(fish): add alias for wsl's user and deno path

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -2,7 +2,17 @@ if test -x /home/linuxbrew/.linuxbrew/bin/brew
   eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 
+if test -x /home/linuxbrew/.linuxbrew/bin/deno
+  export PATH="$HOME/.deno/bin:$PATH"
+end
+
 if type -q exa
   alias ll "exa -l -g --icons"
   alias lla "ll -a"
+end
+
+if test -n "$WSL_DISTRO_NAME"
+  alias open="xdg-open"
+  alias c: "cd /mnt/c"
+  alias d: "cd /mnt/d"
 end
