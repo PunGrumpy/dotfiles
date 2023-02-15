03e089f2ead54d61b5641fbb4238a0afb10447b7
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-02-16 01:50:40 +0700

    feat(nvim): Connect the clipboard between Windows and WSL2

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,10 +6,15 @@ require('grumpy.plugins')
 local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
+local is_wsl = has "wsl"
 
 if is_mac then
   require('grumpy.macos')
 end
 if is_win then
   require('grumpy.windows')
+end
+
+if is_wsl then
+  require('grumpy.wsl')
 end
