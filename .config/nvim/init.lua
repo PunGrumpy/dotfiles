729d5df11bc68e146c077d352eb463ec210bcf5f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-08-23 14:55:27 +0700

    feat(nvim): add linux config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,11 +5,15 @@ require('grumpy.plugins')
 
 local has = vim.fn.has
 local is_mac = has "macunix"
+local is_linux = has "unix"
 local is_win = has "win32"
 local is_wsl = has "wsl"
 
 if is_mac then
   require('grumpy.macos')
+end
+if is_linux then
+  require('grumpy.linux')
 end
 if is_win then
   require('grumpy.windows')
