12bbc5282559a98fed8cd09cdc75b53b81c4eb81
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-08 18:30:48 +0700

    feat(nvim): add null_ls prisma

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -17,10 +17,11 @@ null_ls.setup {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.terraform_fmt,
+    null_ls.builtins.formatting.fish_indent,
+    null_ls.builtins.formatting.prismaFmt,
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
-    null_ls.builtins.formatting.fish_indent,
     null_ls.builtins.diagnostics.fish,
     null_ls.builtins.diagnostics.golangci_lint,
     null_ls.builtins.diagnostics.hadolint,
