7d98c822f99c188331299dc301dea8fc7b4e8f89
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-11-14 14:37:50 +0700

    feat(nvim): update setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,11 @@
-require('grumpy.base')
-require('grumpy.highlights')
-require('grumpy.maps')
-require('grumpy.plugins')
-
-local os = vim.loop.os_uname().sysname
-
-if os == "Darwin" then
-  require('grumpy.macos')
-elseif os == "Linux" then
-  require('grumpy.linux')
-elseif os == "Windows" then
-  require('grumpy.windows')
-else
-  error("Unsupported OS: " .. os)
+if vim.loader then
+  vim.loader.enable()
 end
+
+_G.dd = function(...)
+  require('util.debug').dump(...)
+end
+
+vim.print = _G.dd
+
+require('config.lazy')
