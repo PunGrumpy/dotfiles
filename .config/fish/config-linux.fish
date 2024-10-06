71ef143adac1c40880cb0f0bea70f82e32637dbf
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-07 03:04:40 +0700

    fix(wsl): remove path that broken

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -8,7 +8,4 @@ if test -n "$WSL_DISTRO_NAME"
     alias open wslview
     alias c: "cd /mnt/c"
     alias d: "cd /mnt/d"
-
-    # Remove cursor from PATH (it's a symlink to /mnt/c/Users/PunGrumpy/AppData/Local/Programs/cursor/resources/app/bin)
-    set -gx PATH (string match -v '/mnt/c/Users/PunGrumpy/AppData/Local/Programs/cursor/resources/app/bin' $PATH)
 end
