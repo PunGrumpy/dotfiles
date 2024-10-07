5794b32e0f8331c7b47919b37f15fe471aeade60
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-07 14:21:13 +0700

    feat(wsl): add path for separate vscode and cursor

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -8,4 +8,10 @@ if test -n "$WSL_DISTRO_NAME"
     alias open wslview
     alias c: "cd /mnt/c"
     alias d: "cd /mnt/d"
+
+    # Cursor
+    set -gx PATH /mnt/c/Users/$NAME/AppData/Local/Programs/cursor/resources/app/bin $PATH
+
+    # Code
+    set -gx PATH /mnt/c/Users/$NAME/AppData/Local/Programs/Microsoft\ VS\ Code/bin $PATH
 end
