0bb2ca33a8937ad600432e4c5151d56c38c7652f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-12-29 02:05:00 +0700

    fix(nvim): disabled snacks scroll animations

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -61,15 +61,11 @@ return {
 	},
 
 	-- animations
-	{
-		"echasnovski/mini.animate",
-		event = "VeryLazy",
-		opts = function(_, opts)
-			opts.scroll = {
-				enable = false,
-			}
-		end,
+	"snacks.nvim",
+	opts = {
+		scroll = { enabled = false },
 	},
+	keys = {},
 
 	-- buffer line
 	{
